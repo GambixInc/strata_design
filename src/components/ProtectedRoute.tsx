432c93e41e6388bacc5f6575613cff60782c6132
Ravi Gupta <ravgupta20@gmail.com>
2025-08-10 14:30:54 -0400

    fixed issues with running npm build

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
